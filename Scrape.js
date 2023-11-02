322d8cefd61677d4702a562120e976cf6a435b66
Vaghala <rashlyrash@hotmail.com>
2023-11-02 12:45:38 +0200

    fixing score system

--- Scrape.js
+++ Scrape.js
@@ -13,22 +13,45 @@ for(li of li_elements){
         if(availability.includes("Express παράδοση")){availability = "Express"}
         if(availability == "Διαθέσιμο από 10 έως 30 ημέρες"){continue}
     }
-/*
+
     switch (availability){
-    case "Express":availability = 1; break;
-    case "Διαθέσιμο από 4 έως 10 ημέρες":availability = 10; break;
-    case "Διαθέσιμο": availability = 2;break;
-    case "Διαθέσιμο από 10 έως 30 ημέρες": continue;break;
+    case "Express":
+        score = price+ price*0.1*0.8; 
+        break;
+    case "Διαθέσιμο από 4 έως 10 ημέρες":
+        score = price+ price*0.4*0.8; 
+        break;
+    case "Διαθέσιμο":
+        core = price+ price*0.15*0.8;
+        break;
+    case "Διαθέσιμο από 10 έως 30 ημέρες":
+        break;
     }
-*/
+
     shop = li.querySelectorAll('[class="shop-name"]')[0].innerText;
-    Array_of_Shops.push({"Total_price":price+shipping,"Shop":shop,"Price":price,"Shipping_price":shipping,"Availabillity":availability})
+    Array_of_Shops.push({"Total_price":price+shipping,"Shop":shop,"Price":price,"Shipping_price":shipping,"Availabillity":availability,"Score":score})
 }
 sum =0
 for(a of Array_of_Shops){
     sum = sum + a['Shipping_price']
 }
 
-Array_of_Shops.sort((x,y)=>{return x.Total_price-y.Total_price})
+//Array_of_Shops.sort((x,y)=>{return x.Total_price-y.Total_price})
+Array_of_Shops.sort((x,y)=>{return x.Score-y.Score})
 console.log(Array_of_Shops)
 
+/*
+303 , 4-10
+
+309 , 1
+
+307 , 4 - 10
+
+
+
+price * 0.8
+
+availability * 0.2
+
+1/price *0.8
+*/
